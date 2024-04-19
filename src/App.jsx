cb4487f07376ea7243b1022cb513e1bc87cd4d48
Alaa Anees <alaaaneessaad@gmail.com>
2024-04-19 03:50:52 +0200

    🌐 Yaaay! add bootstrap and tailwind globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="container ">
+      <h1 className="text-4xl text-blue-500 mt-5 bg-danger text-center">
+        Hello, World!
+      </h1>
+      <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4">
+        Click me
+      </button>
+    </div>
+  );
 }
 
 export default App;

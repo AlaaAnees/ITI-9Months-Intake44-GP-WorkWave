acc7f97e65825207186a95838b1d76f7e02f7965
unknown <emaneldeeb1999@gmail.com>
2024-04-24 15:21:19 +0200

    main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./Pages/Layout/Layout";
+import Error from "./Pages/Error/Error";
+
+const routes = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout></Layout>,
+    errorElement: <Error></Error>,
+    children: [],
+  },
+]);
+
 function App() {
-  return (
-    <div className="container ">
-      <h1 className="text-4xl text-blue-500 mt-5 bg-danger text-center">
-        Hello, World!
-      </h1>
-      <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4">
-        Click me
-      </button>
-    </div>
-  );
+  <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;

01598e20ad141b777a3a9c6c4fb45c83c58b211a
Mahmoud Abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2024-04-28 12:02:25 +0300

    📝 Just test
    but i added my own routes in this file Waiting for the merge time

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Pages/Layout/Layout";
 import Error from "./Pages/Error/Error";
+import Home from "./Pages/Home/Home";
+import Chat from "./Pages/Chat/Chat";
+import Chats from "./Pages/Chats/Chats";
 
 const routes = createBrowserRouter([
+  // It may be changed or removed at the time of merging
   {
-    path: "",
+    path: "/",
     element: <Layout></Layout>,
     errorElement: <Error></Error>,
-    children: [],
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        errorElement: <Error></Error>,
+      },
+      {
+        path: "/chats",
+        element: <Chats />,
+        errorElement: <Error></Error>,
+      },
+      {
+        path: "/chat",
+        element: <Chat />,
+        errorElement: <Error></Error>,
+      },
+      {
+        path: "*",
+        element: <Error />,
+        errorElement: <Error></Error>,
+      },
+    ],
   },
 ]);
 
 function App() {
-  <RouterProvider router={routes}></RouterProvider>;
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;

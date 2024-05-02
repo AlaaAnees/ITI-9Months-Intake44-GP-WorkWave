2e471e665b4dc75f244abe0eb9e8902c39a8a719
“Ebrahim1911” <ebrahimn709@hmail.com>
2024-05-02 22:51:02 +0300

    Fix Single Gig fetching Issue

--- src/Pages/SingleGig/SingleGig.jsx
+++ src/Pages/SingleGig/SingleGig.jsx
@@ -1,19 +1,32 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import SingleGigContainer from "../../Components/SingleGig/SingleGigContainer";
-const baseURL = "https://workwave-vq08.onrender.com/api";
+const baseURL = "https://workwave-vq08.onrender.com";
 function SingleGig() {
   const { id } = useParams();
   const [singleGigData, setSingleGig] = useState("");
   useEffect(() => {
     async function fetchSingleGig() {
-      const res = await fetch(`${baseURL}/gigs/single/${id}`);
-      const data = await res.json();
-      setSingleGig(data);
+      try {
+        const res = await fetch(`${baseURL}/api/gigs/single/${id}`);
+        const data = await res.json();
+        setSingleGig(data);
+      } catch (err) {
+        return err;
+      }
     }
     fetchSingleGig();
   }, [id]);
-  return <>{<SingleGigContainer gig={singleGigData} />}</>;
+
+  return (
+    <>
+      {singleGigData ? (
+        <SingleGigContainer gig={singleGigData} />
+      ) : (
+        <p>Loading single gig...</p>
+      )}
+    </>
+  );
 }
 
 export default SingleGig;

b0f59f6c4c61442b980c7d2d311fb5a33f3cfe28
“Ebrahim1911” <ebrahimn709@hmail.com>
2024-05-04 01:57:20 +0300

    Add Reviews Functionality

--- src/Components/Reviews/Review.jsx
+++ src/Components/Reviews/Review.jsx
@@ -1,66 +1,117 @@
 import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 const baseURL = "https://workwave-vq08.onrender.com/api";
-function Review({ gig, id }) {
-  const { userId } = gig;
-  const [ownerData, setOwnerData] = useState("");
-  const [reviews, setReview] = useState("");
-  useEffect(() => {
-    async function fetchOwnerInfo() {
-      try {
-        const res = await fetch(
-          `https://workwave-vq08.onrender.com/api/users/${userId}`
-        );
-        const data = await res.json();
-        setOwnerData(data.data.user);
-      } catch (err) {
-        return err;
-      }
-    }
-    fetchOwnerInfo();
-  }, [userId]);
-
-  useEffect(() => {
-    async function fetchReviews() {
-      const res = await fetch(`${baseURL}/reviews/${id}`);
-      const data = await res.json();
-      setReview(data);
-    }
-    fetchReviews();
-  }, [id]);
-
+function Review({ gig }) {
+  const { reviews } = gig;
+  const [newReview, setNewReview] = useState("");
+  const [desc, setDesc] = useState("");
+  const [star, setStars] = useState(0);
+  async function handlePostReview() {
+    setNewReview("");
+    const token = JSON.parse(localStorage.getItem("token"));
+    const newReview = {
+      gigId: gig._id,
+      desc,
+      star: parseInt(star),
+    };
+    const res = await fetch(`${baseURL}/reviews`, {
+      method: "POST",
+      body: JSON.stringify(newReview),
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    setNewReview(data.data.savedReview);
+  }
   return (
     <>
       {reviews ? (
-        <div className="reviews">
+        reviews.map((rev) => {
+          return (
+            <div className="reviews" key={rev.userId}>
+              <div className="user-holder flex items-center gap-5">
+                <div className="image-holder flex items-center">
+                  <img
+                    src={rev.img}
+                    alt={rev.username}
+                    className="w-10 rounded-full"
+                  />
+                </div>
+                <div className="holder space-y-2">
+                  <div>{rev.username}</div>
+                  <div>
+                    {!isNaN(rev.star) && (
+                      <div className="stars flex items-center">
+                        {Array(Math.round(rev.star))
+                          .fill()
+                          .map((item, i) => (
+                            <FaStar
+                              key={i}
+                              className="text-yellow-400"
+                            ></FaStar>
+                          ))}
+                        <span>{Math.round(rev.star)}</span>
+                      </div>
+                    )}
+                  </div>
+                  <p>{rev.desc}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div>Reviews Is Loading</div>
+      )}
+      {newReview && (
+        <div className="reviews" key={newReview.userId}>
           <div className="user-holder flex items-center gap-5">
             <div className="image-holder flex items-center">
               <img
-                src={ownerData.img}
-                alt={ownerData.username}
+                src={newReview.img}
+                alt={newReview.username}
                 className="w-10 rounded-full"
               />
             </div>
             <div className="holder space-y-2">
-              <div>{ownerData.username}</div>
+              <div>{newReview.username}</div>
               <div>
-                {!isNaN(gig.totalStars / gig.starNumber) && (
+                {!isNaN(newReview.star) && (
                   <div className="stars flex items-center">
-                    {Array(Math.round(gig.totalStars / gig.starNumber))
+                    {Array(Math.round(newReview.star))
                       .fill()
                       .map((item, i) => (
                         <FaStar key={i} className="text-yellow-400"></FaStar>
                       ))}
-                    <span>{Math.round(gig.totalStars / gig.starNumber)}</span>
+                    <span>{Math.round(newReview.star)}</span>
                   </div>
                 )}
               </div>
+              <p>{newReview.desc}</p>
             </div>
           </div>
         </div>
-      ) : (
-        <div>Reviews Is Loading</div>
       )}
+      <div className="add-review">
+        <textarea
+          className="resize-none"
+          placeholder="Review"
+          name="desc"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <input
+          type="number"
+          name="star"
+          min={0}
+          max={5}
+          value={star}
+          onChange={(e) => setStars(e.target.value)}
+        />
+        <button onClick={handlePostReview}>Submit Review</button>
+      </div>
     </>
   );
 }

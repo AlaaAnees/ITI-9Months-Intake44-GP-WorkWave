8cee0a94b43b8bf89ad462820729cfa6ae66f56f
Mahmoud Abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2024-05-15 08:58:25 +0300

    ✅ Here i tested the contact button we will remove it an another time

--- src/Pages/Explore/explore.jsx
+++ src/Pages/Explore/explore.jsx
@@ -1,11 +1,18 @@
 import Contact from "../../Components/Contact/Contact";
 
 const Explore = () => {
+  let ids = {
+    sellerId: "662e9693e3b98a3c9b42c581",
+    buyerId: "6643b8a0773f42b4c6612dab",
+  };
+
+  // const conversationId = "662e9693e3b98a3c9b42c581662ea6c6d385235bd12335ca";
+
   return (
     <>
       <div className="parent h-screen">
         <div className="container">
-          <Contact />
+          <Contact IDs={ids} />
         </div>
       </div>
     </>

25d8c038fa65ba438464b9d1da67967eb072266d
unknown <emaneldeeb1999@gmail.com>
2024-05-16 02:40:51 +0300

    handle uplade images

---
+++ src/Utils/uploadImg.js
@@ -0,0 +1,23 @@
+const upload = async (file) => {
+  const data = new FormData();
+  data.append("file", file);
+  data.append("upload_preset", "workWave");
+  try {
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/diilyjcff/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    const dataUrl = await res.json();
+    console.log(dataUrl, "cloud");
+
+    return dataUrl.secure_url;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+export default upload;
